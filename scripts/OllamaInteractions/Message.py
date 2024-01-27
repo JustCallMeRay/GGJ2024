60c7bb2f0900b9b03263689f49e58918437abed5
Patrick Woodcock <patrick@patrickw.tech>
2024-01-27 14:36:24 +0000

    update Message.py

--- scripts/OllamaInteractions/Message.py
+++ scripts/OllamaInteractions/Message.py
@@ -1,12 +1,13 @@
 import ollama
 
-def send(prompt:str) -> None:
+def send(prompt:str) -> str:
     response = ollama.chat(model='phi', messages=[
     {
         'role': 'user',
         'content': prompt,
     },
     ])
+    return response['message']['content']
 
 def receive() -> str:
     return ""
