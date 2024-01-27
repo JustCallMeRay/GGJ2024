5dd77f08902832106920e2b641d02e88158d5c7e
Patrick Woodcock <patrick@patrickw.tech>
2024-01-27 20:29:34 +0000

    add responses to llm input

--- scripts/OllamaInteractions/Message.py
+++ scripts/OllamaInteractions/Message.py
@@ -3,6 +3,7 @@ try:
 except ImportError :
     print("You don't have olama, please go get it!")
     raise
+import pprint
 
 class Message:
 
@@ -12,20 +13,33 @@ class Message:
         self.previous_messages = []
         pass
 
-    def send(self, prompt:str) -> str:
+    def send(self, prompt:str, role="user") -> str:
+        retries = 10 # retry if we don't get a response
         self.previous_messages.append(
         {
-            'role': 'user',
+            'role': role,
             'content': prompt,
         })
-        response = ollama.chat(model='phi', messages=self.previous_messages)
+        # print("\033[92m Sent: ")
+        # pprint.pprint(self.previous_messages)
+        while True:
+            response = ollama.chat(model='phi', messages=self.previous_messages)
+            retries -= 1
+            if retries < 0 or response['message']['content'] != "":
+                break
+        if response['message']['content'] == "":
+            response['message']['content'] = "I don't know what to say"
+        # print("\033[96m Received: ")
+        # pprint.pprint(response)
+        # print("\033[0m")
+        self.previous_messages.append(response['message'])
         self.last_response = response['message']['content']
         return self.last_response
 
-    def sendNoChat(self, prompt:str) -> str:
+    def sendNoChat(self, prompt:str, role = "user") -> str:
         response = ollama.chat(model='phi', messages=[
         {
-            'role': 'user',
+            'role': role,
             'content': prompt,
         }])
         return response['message']['content']
